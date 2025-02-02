25181a1786f14a41a0db1df3e94a793cc2dd1445
Guanquan18 <s10257825@connect.np.edu.sg>
2025-02-02 20:59:47 +0800

    Modified videoController.js email content

--- controllers/videoCallingController.js
+++ controllers/videoCallingController.js
@@ -33,8 +33,20 @@ const sendUrl = async (req, res) => {
   try{
     const email = 'Staff@gmail.com'; // Get the email from the user object
     const hostRoomUrl = req.body.data.hostRoomUrl; // Get the host room URL from the request body
-    
-    await nodeMailer.sendUrl(email, hostRoomUrl); // Send the OTP email
+    const body = `
+      <div style="font-family: Arial, sans-serif; font-size: 16px;">
+        <h2 style="color: #ED3F3F;">OCBC Help Line Admin Notification</h2>
+        <p>Dear Admin,</p>
+        <p>A new host room has been successfully created for an upcoming customer support video call.</p>
+        <p>You can access the host room using the link below:</p>
+        <p><a href="${hostRoomUrl}" style="color: #ED3F3F; text-decoration: none;">Access the Host Room</a></p>
+        <p>Please ensure the room is managed effectively for the customer's assistance.</p>
+        <br>
+        <p>Best regards,</p>
+        <p><strong>OCBC HelpLine System</strong></p>
+      </div>
+    `;
+    await nodeMailer.sendUrl(email, hostRoomUrl, body); // Send the OTP email
     res.status(200)
   }catch(error){
     await fetch('https://api.whereby.dev/v1/meetings', { method: 'DELETE'});  // Delete the room if an error occurs
